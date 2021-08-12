02f25a4e22174e48d1048b55f6529f50ab185ea2
dodinhhoanganh <ngoanhmanh82@gmail.com>
2021-08-12 12:14:38 +0700

    First commit

--- commands/badapple.js
+++ commands/badapple.js
@@ -1,21 +1,7 @@
 const Discord = require('discord.js');
 const fs = require("fs");
-const MESSAGE_CHAR_LIMIT = 1990;
-
-const splitString = (string, prepend = '', append = '') => {
-  if (string.length <= MESSAGE_CHAR_LIMIT) {
-    return [string];
-  }
-
-  const splitIndex = string.lastIndexOf('\n', MESSAGE_CHAR_LIMIT - prepend.length - append.length);
-  const sliceEnd = splitIndex > 0 ? splitIndex : MESSAGE_CHAR_LIMIT - prepend.length - append.length;
-  const rest = splitString(string.slice(sliceEnd), prepend, append);
-
-  return [`${string.slice(0, sliceEnd)}${append}`, `${prepend}${rest[0]}`, ...rest.slice(1)];
-};
 module.exports.run = async (client, message, args, op) => {
     const msg = await message.channel.send('```Loading...```');
-    const msg1 = await message.channel.send('```Loading...```');
     let i = 1;
  
     var start = +new Date();
@@ -33,9 +19,7 @@ module.exports.run = async (client, message, args, op) => {
           {
              fs.readFile(`badapple\\BA` +  i + '.txt', 'utf8', async function(err, data) {
                   if (err) throw err;
-                    var stringdata = splitString(data);
-                    msg.edit('Render file: BA' + i +'.txt' + '```' + stringdata[0] + '```');
-                    await msg1.edit('```' + stringdata[1] + '```');
+                    msg.edit('Render file: BA' + i +'.txt' + '```' + stringdata[0] + '```', {split: true});
                     console.log(data);
               });
               i++;
